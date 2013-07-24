4a368a7d02ba4e5850c1026e6627ba728584a287
kartikbu <kartikbu@flipkart.com>
2013-07-24 17:48:12 +0530

    Added copyright messaging

--- sample-tcp-proxy/src/main/java/com/flipkart/phantom/sample/handler/ArithmeticTaskHandler.java
+++ sample-tcp-proxy/src/main/java/com/flipkart/phantom/sample/handler/ArithmeticTaskHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.flipkart.phantom.sample.handler;
 
 import com.flipkart.phantom.task.impl.TaskResult;
@@ -7,6 +22,8 @@ import com.flipkart.phantom.task.spi.TaskContext;
 import java.util.Map;
 
 /**
+ * A simple task handler which does basic arithmetic operations.
+ *
  * @author kartikbu
  * @version 1.0
  * @created 24/7/13 12:29 AM
@@ -18,6 +35,10 @@ public class ArithmeticTaskHandler extends HystrixTaskHandler {
     public final String CMD_MUL = "multiply";
     public final String CMD_DIV = "divide";
 
+    /**
+     * Abstract method implementations.
+     * @see com.flipkart.phantom.task.spi.TaskHandler#execute(com.flipkart.phantom.task.spi.TaskContext, String, java.util.Map, byte[])
+     */
     @Override
     public TaskResult execute(TaskContext taskContext, String command, Map<String, String> params, byte[] data) throws RuntimeException {
 
@@ -38,21 +59,37 @@ public class ArithmeticTaskHandler extends HystrixTaskHandler {
 
     }
 
+    /**
+     * Abstract method implementations.
+     * @see com.flipkart.phantom.task.spi.TaskHandler#getName()
+     */
     @Override
     public String getName() {
         return "Arithmetic";
     }
 
+    /**
+     * Abstract method implementations.
+     * @see com.flipkart.phantom.task.spi.TaskHandler#shutdown(com.flipkart.phantom.task.spi.TaskContext)
+     */
     @Override
     public void shutdown(TaskContext taskContext) throws Exception {
 
     }
 
+    /**
+     * Abstract method implementations.
+     * @see com.flipkart.phantom.task.spi.TaskHandler#getCommands()
+     */
     @Override
     public String[] getCommands() {
         return new String[]{CMD_ADD,CMD_SUB,CMD_MUL,CMD_DIV};  //To change body of implemented methods use File | Settings | File Templates.
     }
 
+    /**
+     * Abstract method implementations.
+     * @see com.flipkart.phantom.task.spi.HystrixTaskHandler#getFallBack(com.flipkart.phantom.task.spi.TaskContext, String, java.util.Map, byte[])
+     */
     @Override
     public TaskResult getFallBack(TaskContext taskContext, String command, Map<String, String> params, byte[] data) {
         return null;
