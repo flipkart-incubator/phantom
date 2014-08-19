a54f5ed058d9c413aed5970127f541ecf4087af0
ARYA KETAN <arya.ketan@flipkart.com>
2014-08-19 16:46:45 +0530

    Fix for Thread pool TCP Netty Server  having an unbounded worker queue and is without any thread pool management r:phaneesh

--- runtime/src/main/java/com/flipkart/phantom/runtime/impl/server/netty/TCPNettyServer.java
+++ runtime/src/main/java/com/flipkart/phantom/runtime/impl/server/netty/TCPNettyServer.java
@@ -21,89 +21,98 @@ import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * <code>TCPNettyServer</code> is a concrete implementation of the {@link AbstractNettyNetworkServer} for {@link TRANSMISSION_PROTOCOL#TCP}
- * 
+ *
  * @author Regunath B
  * @version 1.0, 15 Mar 2013
  */
 public class TCPNettyServer extends AbstractNettyNetworkServer {
-	
-	/** The default counts (invalid one) for server and worker pool counts*/
-	private static final int INVALID_POOL_SIZE = -1;
-	
-	/** The server and worker thread pool sizes*/
-	private int serverPoolSize = INVALID_POOL_SIZE;
-	private int workerPoolSize = INVALID_POOL_SIZE;
 
-	/** The server and worker ExecutorService instances*/
-	private ExecutorService serverExecutors;
-	private ExecutorService workerExecutors;
+    /** The default counts (invalid one) for server and worker pool counts*/
+    private static final int INVALID_POOL_SIZE = -1;
+
+    /** The server and worker thread pool sizes*/
+    private int serverPoolSize = INVALID_POOL_SIZE;
+    private int workerPoolSize = INVALID_POOL_SIZE;
+
+    /** The server and worker ExecutorService instances*/
+    private ExecutorService serverExecutors;
+    private ExecutorService workerExecutors;
 
     /** Server Type */
     private String serverType = "TCP Netty Server";
 
 
-	/**
-	 * Interface method implementation. Returns {@link TRANSMISSION_PROTOCOL#TCP}
-	 * @see com.flipkart.phantom.runtime.spi.server.NetworkServer#getTransmissionProtocol()
-	 */
-	public TransmissionProtocol getTransmissionProtocol() {
-		return TRANSMISSION_PROTOCOL.TCP;
-	}
+    /**
+     * Interface method implementation. Returns {@link TRANSMISSION_PROTOCOL#TCP}
+     * @see com.flipkart.phantom.runtime.spi.server.NetworkServer#getTransmissionProtocol()
+     */
+    public TransmissionProtocol getTransmissionProtocol() {
+        return TRANSMISSION_PROTOCOL.TCP;
+    }
 
-	/**
-	 * Interface method implementation. Creates server and worker thread pools if required and then calls {@link #afterPropertiesSet()} on the super class
-	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
-	 */
-	public void afterPropertiesSet() throws Exception {		
-		if (this.getServerExecutors() == null) { // no executors have been set for server listener
-			if (this.getServerPoolSize() != TCPNettyServer.INVALID_POOL_SIZE) { // thread pool size has been set. create and use a fixed thread pool
-				this.setServerExecutors(Executors.newFixedThreadPool(this.getServerPoolSize(), new NamedThreadFactory("TCPServer-Listener")));
-			} else { // default behavior of creating and using a cached thread pool
-				this.setServerExecutors(Executors.newCachedThreadPool(new NamedThreadFactory("TCPServer-Listener")));
-			}
-		}
-		if (this.getWorkerExecutors() == null) {  // no executors have been set for workers
-			if (this.getWorkerPoolSize() != TCPNettyServer.INVALID_POOL_SIZE) { // thread pool size has been set. create and use a fixed thread pool
-				this.setWorkerExecutors(Executors.newFixedThreadPool(this.getWorkerPoolSize(), new NamedThreadFactory("TCPServer-Worker")));
-			}else { // default behavior of creating and using a cached thread pool
-				this.setWorkerExecutors(Executors.newCachedThreadPool(new NamedThreadFactory("TCPServer-Worker")));
-			}
-		}
-		super.afterPropertiesSet();
-	}
+    /**
+     * Interface method implementation. Creates server and worker thread pools if required and then calls {@link #afterPropertiesSet()} on the super class
+     * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+     */
+    public void afterPropertiesSet() throws Exception {
+        if (this.getServerExecutors() == null) { // no executors have been set for server listener
+            if (this.getServerPoolSize() == TCPNettyServer.INVALID_POOL_SIZE) { // thread pool size has been set. create and use a fixed thread pool
+                this.setServerPoolSize(Runtime.getRuntime().availableProcessors());
+            }
+            this.setServerExecutors(new ThreadPoolExecutor(this.getServerPoolSize(),
+                    this.getServerPoolSize() * 4,
+                    30,
+                    TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<Runnable>(this.getServerPoolSize() * 12),
+                    new NamedThreadFactory("TCPServer-Listener"),
+                    new ThreadPoolExecutor.CallerRunsPolicy()));
+        }
+        if (this.getWorkerExecutors() == null) {  // no executors have been set for workers
+            if (this.getWorkerPoolSize() == TCPNettyServer.INVALID_POOL_SIZE) { // thread pool size has been set. create and use a fixed thread pool
+                this.setWorkerPoolSize(Runtime.getRuntime().availableProcessors());
+            }
+            this.setWorkerExecutors(new ThreadPoolExecutor(this.getWorkerPoolSize(),
+                    this.getWorkerPoolSize() * 4,
+                    30,
+                    TimeUnit.SECONDS,
+                    new LinkedBlockingQueue<Runnable>(this.getWorkerPoolSize() * 12),
+                    new NamedThreadFactory("TCPServer-Worker"),
+                    new ThreadPoolExecutor.CallerRunsPolicy()));
+        }
+        super.afterPropertiesSet();
+    }
 
-	/**
-	 * Overriden super class method. Returns a readable string for this TCPNettyServer
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString(){
-		return "TCPNettyServer [socketAddress=" + socketAddress + ", portNumber=" + portNumber + "] " + this.getPipelineFactory();
-	}
-	
-	/**
-	 * Interface method implementation. Creates and returns a Netty ServerBootstrap instance
-	 * @see com.flipkart.phantom.runtime.impl.server.netty.AbstractNettyNetworkServer#createServerBootstrap()
-	 */
-	protected Bootstrap createServerBootstrap() throws RuntimeException {
-		return new ServerBootstrap(new NioServerSocketChannelFactory(this.getServerExecutors(), this.getWorkerExecutors()));
-	}
+    /**
+     * Overriden super class method. Returns a readable string for this TCPNettyServer
+     * @see java.lang.Object#toString()
+     */
+    public String toString(){
+        return "TCPNettyServer [socketAddress=" + socketAddress + ", portNumber=" + portNumber + "] " + this.getPipelineFactory();
+    }
 
-	/**
-	 * Abstract method implementation. Creates and returns a Netty Channel from the ServerBootstrap that was
-	 * previously created in {@link TCPNettyServer#createServerBootstrap()}
-	 * @see com.flipkart.phantom.runtime.impl.server.netty.AbstractNettyNetworkServer#createChannel()
-	 */
-	protected Channel createChannel() throws RuntimeException {
-		if (this.getServerBootstrap() == null) {
-			throw new RuntimeException("Error creating Channel. Bootstrap instance cannot be null. See TCPNettyServer#createServerBootstrap()");
-		}
-		return ((ServerBootstrap)this.serverBootstrap).bind(this.socketAddress);
-	}
+    /**
+     * Interface method implementation. Creates and returns a Netty ServerBootstrap instance
+     * @see com.flipkart.phantom.runtime.impl.server.netty.AbstractNettyNetworkServer#createServerBootstrap()
+     */
+    protected Bootstrap createServerBootstrap() throws RuntimeException {
+        return new ServerBootstrap(new NioServerSocketChannelFactory(this.getServerExecutors(), this.getWorkerExecutors()));
+    }
+
+    /**
+     * Abstract method implementation. Creates and returns a Netty Channel from the ServerBootstrap that was
+     * previously created in {@link TCPNettyServer#createServerBootstrap()}
+     * @see com.flipkart.phantom.runtime.impl.server.netty.AbstractNettyNetworkServer#createChannel()
+     */
+    protected Channel createChannel() throws RuntimeException {
+        if (this.getServerBootstrap() == null) {
+            throw new RuntimeException("Error creating Channel. Bootstrap instance cannot be null. See TCPNettyServer#createServerBootstrap()");
+        }
+        return ((ServerBootstrap)this.serverBootstrap).bind(this.socketAddress);
+    }
 
     /**
      * Abstract method implementation. Returns server type as string.
@@ -123,31 +132,31 @@ public class TCPNettyServer extends AbstractNettyNetworkServer {
         return ""+this.portNumber;
     }
 
-	/** Start Getter/Setter methods */
-	public int getServerPoolSize() {
-		return this.serverPoolSize;
-	}
-	public void setServerPoolSize(int serverPoolSize) {
-		this.serverPoolSize = serverPoolSize;
-	}
-	public int getWorkerPoolSize() {
-		return this.workerPoolSize;
-	}
-	public void setWorkerPoolSize(int workerPoolSize) {
-		this.workerPoolSize = workerPoolSize;
-	}
-	public ExecutorService getServerExecutors() {
-		return this.serverExecutors;
-	}
-	public void setServerExecutors(ExecutorService serverExecutors) {
-		this.serverExecutors = serverExecutors;
-	}
-	public ExecutorService getWorkerExecutors() {
-		return this.workerExecutors;
-	}
-	public void setWorkerExecutors(ExecutorService workerExecutors) {
-		this.workerExecutors = workerExecutors;
-	}	
-	/** End Getter/Setter methods */
-	
+    /** Start Getter/Setter methods */
+    public int getServerPoolSize() {
+        return this.serverPoolSize;
+    }
+    public void setServerPoolSize(int serverPoolSize) {
+        this.serverPoolSize = serverPoolSize;
+    }
+    public int getWorkerPoolSize() {
+        return this.workerPoolSize;
+    }
+    public void setWorkerPoolSize(int workerPoolSize) {
+        this.workerPoolSize = workerPoolSize;
+    }
+    public ExecutorService getServerExecutors() {
+        return this.serverExecutors;
+    }
+    public void setServerExecutors(ExecutorService serverExecutors) {
+        this.serverExecutors = serverExecutors;
+    }
+    public ExecutorService getWorkerExecutors() {
+        return this.workerExecutors;
+    }
+    public void setWorkerExecutors(ExecutorService workerExecutors) {
+        this.workerExecutors = workerExecutors;
+    }
+    /** End Getter/Setter methods */
+
 }
